2c886f2629dfb6a7ce1173826223023ae70fb007
WangYong22 <wangyong2@gmail.com>
2024-04-15 23:34:47 +0800

    Initial Commit

---
+++ MacbookiOStest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacbookiOStest1
+//
+//  Created by test on 2024/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacbookiOStest1/MacbookiOStest1App.swift
@@ -0,0 +1,17 @@
+//
+//  MacbookiOStest1App.swift
+//  MacbookiOStest1
+//
+//  Created by test on 2024/4/15.
+//
+
+import SwiftUI
+
+@main
+struct MacbookiOStest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
